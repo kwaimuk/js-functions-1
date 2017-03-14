37715a512511f68d0201669ed743aa0521144339
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-13 18:43:42 -1000

    partial done

--- functions.js
+++ functions.js
@@ -3,7 +3,13 @@
  * @param {number} n
  * @return {string} the number as a string
  */
+function numberToString(Number){
+	Number= Number.toString();
+	return n=Number;
+}
 
+var n =15;
+numberToString(n);
 
 /**
  * Adds one to a given number.
@@ -11,13 +17,21 @@
  * @return {number}
  */
 
-
+function increase(bin){
+	 bin++;
+	 return bin;
+}
+ increase(n);
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
-
+function decrease(bin){
+	 bin--;
+	 return bin;
+}
+ decrease(n);
 
 /**
  * Adds two numbers.
@@ -25,7 +39,15 @@
  * @param {number} y
  * @return {number} the sum
  */
+ function add(gee,ree){
+ 	var sum= 0;
+ 	sum= gee + ree;
+ 	return sum;
 
+ }
+ var x= 1;
+ var y= 2;
+add(x,y);
 
 /**
  * Subtracts the second number from the first.
@@ -33,7 +55,15 @@
  * @param {number} y
  * @return {number} the difference
  */
+ function subtract(gee,ree){
+ 	var minus= 0;
+ 	minus= gee - ree;
+ 	return minus;
 
+ }
+ var x= 1;
+ var y= 2;
+subtract(x,y);
 
 /**
  * Multiplies two numbers.
@@ -41,7 +71,15 @@
  * @param {number} y
  * @return {number} the product
  */
+ function multiply(gee,ree){
+ 	var product= 0;
+ 	product= gee * ree;
+ 	return product;
 
+ }
+ var x= 1;
+ var y= 2;
+multiply(x,y);
 
 /**
  * Divides the first number by the second.
@@ -49,14 +87,29 @@
  * @param {number} y
  * @return {number} the quotient
  */
+function divide(gee,ree){
+ 	var division= 0;
+ 	division= gee / ree;
+ 	return division;
 
+ }
+ var x= 1;
+ var y= 2;
+divide(x,y);
 
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
+function square(glee){
+ 	var squarie= 0;
+ 	squarie= glee * glee;
+ 	return squarie;
 
+ }
+ var x= 4;
+square(x);
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -66,7 +119,9 @@
  * @param {number} y
  * @return {number} the result
  */
+function calculate(x,y,z){
 
+}
 
 /**
  * Returns true if `a` is greater than `b`.
@@ -74,6 +129,13 @@
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
+function isGreaterThan(a,b){
+	return a>b;
+	
+}
+var gee= 6;
+var pee= 5;
+isGreaterThan(gee,pee);
 
 
 /**
@@ -82,7 +144,13 @@
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
+function isLessThan(a,b){
+return a<b;
+}
 
+var lee= 7;
+var pee= 8;
+isLessThan(lee,pee);
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -90,7 +158,17 @@
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+function areEqual(lee,pee){
+	if(lee===pee){
+		status = true;
+		return status;
+	}
+}
 
+var status;
+var a= 8;
+var b= 8;
+areEqual(a,b);
 
 /**
  * Returns the smallest value of two numbers.
@@ -98,6 +176,18 @@
  * @param {number} y
  * @return {number} the smallest number
  */
+var smallest =0;
+var d = 4;
+var g = 6;
+function minimum(pow,wow){
+	if(pow>wow){
+		smallest=wow;
+	}else{
+		smallest=pow;
+	}
+
+	return smallest;
+}
 
 
 /**
@@ -106,14 +196,32 @@
  * @param {number} y
  * @return {number} the largest number
  */
+var largest =0;
+var d = 4;
+var g = 6;
+function maximum(pow,wow){
+	if(pow<wow){
+		largest=wow;
+	}else{
+		largest=pow;
+	}
 
+	return largest;
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-
+var eEven=0;
+var n =4;
+function isEven(numbah){
+	if(numbah%2===0){
+		eEven= true;
+	}
+	return eEven;
+}
 
 /**
  * Returns true if `n` is odd.
@@ -121,7 +229,14 @@
  * @return {boolean} the number is odd
  */
 
-
+var oOdd=0;
+var n =9;
+function isOdd(numbah){
+	if(numbah%2===1){
+		oOdd= true;
+	}
+	return oOdd;
+}
 /**
  * Returns a letter grade.
  * "A": 90-100%
@@ -150,7 +265,15 @@
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
+var one ="all";
+var two ="aboard!";
+var container=0;
+function combine(word1,word2){
+	container = word1 +" "+ word2;
+	return container;
 
+}
+combine(one,two);
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
